a1a463ab821137d8e9e42b5819db8c509f8055f7
RanjithRyan00 <ranjithrazzy@gmail.com>
2024-04-11 15:06:45 +0530

    created by ranjith

---
+++ Routes/userRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const {
+  loginController,
+  registerController,
+  fetchAllUsersController,
+} = require("../Controllers/UserController");
+
+const { protect } = require("../middleware/authMiddleware");
+
+const Router = express.Router();
+
+Router.post("/login", loginController);
+Router.post("/register", registerController);
+Router.get("/fetchUsers", protect, fetchAllUsersController);
+module.exports= Router;
